2c1b9dcd2af5e358da19eb6460bd014686e557e8
Ricardo Larcher Pires <13319775+RicardoTheCoder@users.noreply.github.com>
2021-07-21 15:37:04 +0100

    Delete Guu.API directory

--- Guu.API/Items/Others/OtherItem.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Guu.API.Others
-{
-	/// <summary>
-	/// This is the base class for all other items
-	/// </summary>
-	public abstract class OtherItem : RegistryItem<OtherItem> { }
-}
--- Guu.API/Items/UI/UIItem.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Guu.API.UI
-{
-	/// <summary>
-	/// This is the base class for all ui items
-	/// </summary>
-	public abstract class UIItem : RegistryItem<UIItem> { }
-}
